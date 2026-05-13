8683f874953d4e6769cd0f49f409fb9e91e873d9
snuteg <snuteg@gmail.com>
2026-05-13 10:35:54 +0200

    Add start method in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        String filename = "books.txt";
+        FileBookRepository repository = new FileBookRepository(filename);
+        BookService service = new BookService(repository);
 
+        AppMenu app = new AppMenu(service);
+        app.start();
     }
 }
