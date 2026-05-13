7bd32b28a6fc8881c98211f00c2379ea33bcb715
snuteg <snuteg@gmail.com>
2026-05-13 11:56:28 +0200

    Delete findByReleaseDate and findByRating methods

--- src/BookRepository.java
+++ src/BookRepository.java
@@ -4,8 +4,6 @@ public interface BookRepository {
     void save(Book book);
     void delete(Book book);
     List<Book> findAll();
-    Book findByReleaseDate(int releaseDate);
-    Book findByRating(double rating);
     void saveToFile();
     void loadFromFile();
 }
