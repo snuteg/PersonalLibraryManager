2b33b1b88975279feb1208493a36688fb7ea3a7c
snuteg <snuteg@gmail.com>
2026-05-13 10:07:59 +0200

    Fix save and load in  FileBookRepository

--- src/FileBookRepository.java
+++ src/FileBookRepository.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileBookRepository implements BookRepository {
     private String filename;
     private List<Book> books = new ArrayList<>();
+    private int nextId = 1;
 
     public FileBookRepository(String filename) {
         this.filename = filename;
@@ -11,6 +15,7 @@ public class FileBookRepository implements BookRepository {
 
     @Override
     public void save(Book book) {
+        book.setId(nextId++);
         books.add(book);
     }
 
@@ -84,13 +89,59 @@ public class FileBookRepository implements BookRepository {
         return null;
     }
 
+    public void setBooks(List<Book> newBooks) {
+        books.clear();
+        books.addAll(newBooks);
+
+        int maxId = 0;
+        for (Book book : books) {
+            if (book.getId() > maxId) {
+                maxId = book.getId();
+            }
+        }
+
+        nextId = maxId + 1;
+    }
+
     @Override
     public void saveToFile() {
+        StringBuilder lines = new StringBuilder();
 
+        for (Book book : books) {
+            lines.append(book.forFileString());
+        }
+
+        try {
+            Files.writeString(Path.of(filename), lines);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void loadFromFile() {
+        Book book;
+        List<Book> newBooks = new ArrayList<>();
 
+        try {
+            String lines = Files.readString(Path.of(filename));
+            String[] parts = lines.split("\n");
+
+            for (String part : parts) {
+                String[] splitParts = part.split(";");
+                String title = splitParts[0];
+                String author = splitParts[1];
+                int releaseDate = Integer.parseInt(splitParts[2]);
+                String genre = splitParts[3];
+                BookStatus status = BookStatus.valueOf(splitParts[4]);
+                double rating = Double.parseDouble(splitParts[5]);
+                book = new Book(title, author, releaseDate, genre, status, rating);
+                newBooks.add(book);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        setBooks(newBooks);
     }
 }
